d8cee8fcaa22b132af3a2469249b1e70e05af194
Stephen McCullough <steve@shopkeep.com>
2016-01-08 13:00:36 +0000

    Minor Tidyup

--- spec/links_spec.rb
+++ spec/links_spec.rb
@@ -7,23 +7,23 @@ hoursintheday - https://medium.com/@itsmeduncan/you-only-have-so-many-hours-in-t
 " 
   end
 
-  it 'lists all the urls it should' do
+  before do
     stub_const("Links::LINK_FILE", 'spec/fixtures/links.txt')
+  end
+
+  it 'lists all the urls it should' do
     expect(Links.cat_file).to eq links 
   end
 
   it 'returns the correct url when searching' do
-    stub_const("Links::LINK_FILE", 'spec/fixtures/links.txt')
     expect(Links.search_file('msh').first.chomp).to eq 'msh - http://hertzber.gs/'
   end
 
   it 'returns the last n entries when tailing' do
-    stub_const("Links::LINK_FILE", 'spec/fixtures/links.txt')
     expect(Links.tail_file(1).chomp).to eq 'hoursintheday - https://medium.com/@itsmeduncan/you-only-have-so-many-hours-in-the-day-a8c004a21d72#.kk2gwrcu0'
   end
 
   it 'returns the correct timestamp' do 
-    stub_const("Links::LINK_FILE", 'spec/fixtures/links.txt')
     expect(Links.timestamp).to eq Time.now.strftime("%A %Y-%m-%d %H:%M") 
   end
 end
