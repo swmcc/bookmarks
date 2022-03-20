d8bb016ffb51e9fee1609e15cc314b8b5f8cd824
Stevie Codes <steviecodesyeshedoes@gmail.com>
2022-03-20 03:42:51 +0000

    Enabled.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,5 @@
 class LinksController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_link, only: %i[ show edit update destroy ]
 
   # GET /links or /links.json
