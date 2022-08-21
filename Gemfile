6bc453d180fa3cd65be0043ac12fc68bf366e68f
Stevie McCullough <stephen.mccullough@gmail.com>
2022-08-21 10:29:38 +0100

    [NO_TICKET] - Added administrate

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.0'
 
+gem 'administrate'
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'jbuilder', '~> 2.7'
 gem 'pg'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :links
+
+      root to: "links#index"
+    end
   resources :links
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "links#new"
