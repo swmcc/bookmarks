422b3cb0d7a79ef78b4efa7c19d901f229c46fec
Stevie McCullough <stephen.mccullough@gmail.com>
2022-11-27 19:15:40 +0000

    #14 - Upgraded to rails 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   namespace :admin do
-      resources :links
-
-      root to: "links#index"
-    end
+    resources :links
+    root to: 'links#index'
+  end
   resources :links
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "links#new"
-
-  get '/search', to: "links#search"
+  root to: "links#index"
+  get '/search', to: "links#search"  
 end
